fda1302bb7b27b7ea74be9dede198983b185cfff
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-04-02 11:52:59 +0200

    question.js creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import "./question";
 import { useState } from "react";
 import "./App.css";
+import { quiz } from "./question";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>Popo</h1>
+      <p>{console.log(quiz)};</p>
     </>
   );
 }
